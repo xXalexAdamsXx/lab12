a0f0be9b275b42fc5f09eb233da1711fe992e512
Alex Adams <alexadams6@icloud.com>
2023-11-17 15:03:06 -0800

    updated TreeNode class

--- src/Trees/FamilyTree.java
+++ src/Trees/FamilyTree.java
@@ -29,26 +29,24 @@ public class FamilyTree
         }
         
         
-        void addChild(TreeNode childNode)
-        {
-            // Add childNode to this node's children list. Also
-            // set childNode's parent to this node.
+        void addChild(TreeNode childNode) {
+        	children.add(childNode);
+        	childNode.parent = this;
         }
         
         
         // Searches subtree at this node for a node
         // with the given name. Returns the node, or null if not found.
-        TreeNode getNodeWithName(String targetName)
-        {
+        TreeNode getNodeWithName(String targetName){
             // Does this node have the target name?
-            if (?????)
+            if (this.name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
-                // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+            	if (this.name.equals(child.name))
+                    return child;
             }
             
             // Not found anywhere.
@@ -62,11 +60,11 @@ public class FamilyTree
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
 
-            // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
-            // the nodes of a tree is like traversing a linked list. If that isn’t clear,
-            // draw a tree, mark any leaf node, and then mark its ancestors in order from
-            // recent to ancient. Expect a question about this on the final exam.
-
+            TreeNode traverse = this.parent;
+            while(traverse != null) {
+            	ancestors.add(traverse);
+            	traverse = traverse.parent;
+            }
             return ancestors;
         }
         
